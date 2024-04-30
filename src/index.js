a3e2c757b449650e1930a7922fe13897e8314a8b
Tobias Alves <tobiasjose12@gmail.com>
2024-04-30 09:26:04 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+import './style.css';
+
+ReactDom.render (
+  <div className="container">
+    <App />
+  </div>,
+  document.getElementById ('root')
+);
